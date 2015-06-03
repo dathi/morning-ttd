854ffff3e5c223e2616cda273ba0bff1a3fa7057
dathi <dathi@tma.com.vn>
2015-06-03 17:06:34 +0700

    implement When, update Index view and pass When in defs steps

--- features/step_definitions/numerals_steps.rb
+++ features/step_definitions/numerals_steps.rb
@@ -3,7 +3,7 @@ Given(/^I am on the main page$/) do
 end
 
 When(/^I enter (\d+) into my field$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+  fill_in 'number', :with => arg1
 end
 
 When(/^I press submit$/) do
