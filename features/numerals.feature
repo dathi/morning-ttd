041866788b567ad4e27338bdfcf2e52d395179dd
dathi <dathi@tma.com.vn>
2015-06-03 16:30:05 +0700

    add def steps file

--- features/numerals.feature
+++ features/numerals.feature
@@ -1,5 +1,6 @@
-Scenario: Display a number below 12 in written form
-  Given I am on the main page
-  When I enter 10 into my field
-  And I press submit
-  Then I see "ten" as a result
+Feature: Count number
+  Scenario: Display a number below 12 in written form
+    Given I am on the main page
+    When I enter 10 into my field
+    And I press submit
+    Then I see "ten" as a result
---
+++ features/step_definitions/numerals_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I am on the main page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I enter (\d+) into my field$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press submit$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see "(.*?)" as a result$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
